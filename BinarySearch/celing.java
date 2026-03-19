916c02e23e265159dd3ddbac0c07ebd9d0e6544d
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-03-19 14:59:25 +0530

    small change

--- BinarySearch/celing.java
+++ BinarySearch/celing.java
@@ -19,7 +19,7 @@ public class celing {
     }
     public static int BS_Celing(int[]arr,int target){
         int start =0;
-        int end = arr.length;
+        int end = arr.length-1;
         while(start <=end){
             int mid = start+(end-start)/2;
             if(arr[mid]<target){
