8119e742dfa2986478bb8e0401f4801f2f2fbeea
vishwakbalaji-a <vishwakbalaji2304@gmail.com>
2026-03-31 12:24:16 +0530

    Selection Sort

---
+++ sorting/SelectionSort.java
@@ -0,0 +1,24 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+    public static void main(String[] args) {
+        int [] arr = {5,4,3,2,1};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void selectionSort(int [] arr){
+        for(int i = 0;i < arr.length;i++){
+            int minIndex = i;
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[j]<arr[minIndex]){
+                    minIndex=j;
+                }
+            }
+            int temp = arr[minIndex];
+            arr[minIndex]=arr[i];
+            arr[i]=temp;
+        }
+    }
+}
